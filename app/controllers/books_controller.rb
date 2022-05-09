cf1fb62329fb3a5bb4673c2953450bb971af7e32
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-05-09 14:57:39 +0000

    [Finish] validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created." 
       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
     end
-    flash[:notice] = "Book was successfully created." 
   end
 
   def show
@@ -26,18 +26,18 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
     end
-    flash[:notice] = "Book was successfully updated."
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
     flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
   
   
