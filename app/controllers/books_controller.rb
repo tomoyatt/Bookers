9e9b17647e61946306e57e00c3047c1d8c420192
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-05-09 14:24:37 +0000

    [Add] validates

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+    flash[:notice] = "Book was successfully created." 
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+    flash[:notice] = "Book was successfully updated."
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
+  end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
